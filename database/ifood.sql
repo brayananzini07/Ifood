30d79f115fe2b3c7c7901193f722e6e5c0949b2f
brayan <brayan_anzini@estudante.sesisenai.org.br>
2026-08-27 12:17:19 -0300

    add- adiciona arquivos iniciais para gerenciamento de clientes, pedidos e restaurantes

---
+++ database/ifood.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE ifood;
+USE ifood;
+
+CREATE TABLE clientes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    telefone VARCHAR(15),
+);
+
+CREATE TABLE restaurantes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    endereco VARCHAR(255),
+    telefone VARCHAR(15),
+    categoria VARCHAR(50)
+);
+
+CREATE TABLE pedidos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    cliente_id INT,
+    restaurante_id INT,
+    data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY (restaurante_id) REFERENCES restaurantes(id)
+);
