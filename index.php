64577318c67ef90eec705507c60fc1f165418fa4
brayan <brayan_anzini@estudante.sesisenai.org.br>
2026-08-27 12:29:31 -0300

    add- cria a estrutura inicial do sistema de pedidos

---
+++ index.php
@@ -0,0 +1,21 @@
+<h1>iFood - Sistema de Pedidos</h1>
+
+<h2>Clientes</h2>
+
+<a href="Clientes/cadastrar_cliente.php">Cadastrar cliente</a>
+<br>
+<a href="Clientes/consultar_cliente.php">Consultar clientes</a>
+
+<h2>Restaurantes</h2>
+
+<a href="restaurante/cadastrar_restaurante.php">Cadastrar restaurante</a>
+<br>
+<a href="restaurante/consultar_restaurante.php">Consultar restaurantes</a>
+
+<h2>Pedidos</h2>
+
+<a href="pedidos/cadastrar_pedido.php">Cadastrar pedido</a>
+<br>
+<a href="pedidos/consultar_pedido.php">Consultar pedidos</a>
+<br>
+<a href="pedidos/por_cliente.php">Pedidos por cliente</a>
